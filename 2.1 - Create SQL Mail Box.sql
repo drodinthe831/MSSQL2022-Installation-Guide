a41a009f14302a6704cb4cab14f6997d1782beee
dave@dbsecops <dave@dbsecops.com>
2026-08-02 14:13:24 -0700

    initial update

---
+++ 2.1 - Create SQL Mail Box.sql
@@ -0,0 +1,110 @@
+/***************************************************************************************************
+Procedure:          Create Mailbox
+Create Date:        2024-02-29
+Author:             Dave Rodriguez
+Description:        Pull existing SQL Server Mail Settings
+Call by:            [2.0 - Pull SQL Mail Param.sql]
+Affected table(s):  1 of 3 Script to create Mail Box
+Used By:            DCSIT DevOps
+Parameter(s):       @param1 - description and usage
+                    @param2 - description and usage
+Usage:              EXEC dbo.sp_DoSomeStuff
+                        @param1 = 1,
+                        @param2 = 3,
+                        @param3 = 2
+                    Additional notes or caveats about this object, like where is can and cannot be run, or
+                    gotchas to watch for when using it.
+****************************************************************************************************
+SUMMARY OF CHANGES
+Date(yyyy-mm-dd)    Author              Comments
+------------------- ------------------- ------------------------------------------------------------
+2024-02-29          Dave Rodriguez      upload to Gitlab.
+***************************************************************************************************/
+
+
+DECLARE @sqlserver varchar(20);
+
+-- SET TO SQL SERVER you would like to pull settings from. 
+
+SET @sqlserver = 'PRESCIO330DB154';
+
+use master 
+go 
+sp_configure 'show advanced options',1 
+go 
+reconfigure with override 
+go 
+	sp_configure 'Database Mail XPs',1 
+	--go 
+	--sp_configure 'SQL Mail XPs',0 
+	go 
+	reconfigure 
+	go 
+	 
+	-------------------------------------------------------------------------------------------------- 
+	-- BEGIN Mail Settings DBMAIL 
+	-------------------------------------------------------------------------------------------------- 
+	IF NOT EXISTS(SELECT * FROM msdb.dbo.sysmail_profile WHERE  name = 'DBMAIL')  
+	  BEGIN 
+	    --CREATE Profile [DBMAIL] 
+	    EXECUTE msdb.dbo.sysmail_add_profile_sp 
+	      @profile_name = 'DBMAIL', 
+	      @description  = ''; 	  
+	END --IF EXISTS profile 
+	   
+	  IF NOT EXISTS(SELECT * FROM msdb.dbo.sysmail_account WHERE  name = @sqlserver) 
+	  BEGIN 
+	    --CREATE Account [STAGCIO330DB154] 
+	    EXECUTE msdb.dbo.sysmail_add_account_sp 
+	    @account_name            = UCASE(SELECT @sqlserver), 
+	    @email_address           = LCASE(SELECT @sqlserver + '@dliflc.edu'), 
+	    @display_name            = LCASE(SELECT @sqlserver + '@dliflc.edu'), 
+	    @replyto_address         = LCASE(SELECT @sqlserver + '@dliflc.edu'), 
+	    @description             = '', 
+	    @mailserver_name         = 'STAGCIO330EX030', 
+	    @mailserver_type         = 'SMTP', 
+	    @port                    = '25', 
+	    @username                =  NULL , 
+	    @password                =  NULL ,  
+	    @use_default_credentials =  1 , 
+	    @enable_ssl              =  0 ; 
+	  END --IF EXISTS  account 
+	   
+	IF NOT EXISTS(SELECT * 
+	              FROM msdb.dbo.sysmail_profileaccount pa 
+	                INNER JOIN msdb.dbo.sysmail_profile p ON pa.profile_id = p.profile_id 
+	                INNER JOIN msdb.dbo.sysmail_account a ON pa.account_id = a.account_id   
+	              WHERE p.name = 'DBMAIL' 
+	                AND a.name = @sqlserver)  
+	  BEGIN 
+	    -- Associate Account [STAGCIO330DB154] to Profile [DBMAIL] 
+	    EXECUTE msdb.dbo.sysmail_add_profileaccount_sp 
+	      @profile_name = 'DBMAIL', 
+		  @account_name = UCASE(SELECT @sqlserver), 
+		@sequence_number = 1 ; 
+	  END 
+
+
+	--IF EXISTS associate accounts to profiles 
+	--------------------------------------------------------------------------------------------------- 
+	-- Drop Settings For DBMAIL 
+	-------------------------------------------------------------------------------------------------- 
+	/* 
+	IF EXISTS(SELECT * 
+	            FROM msdb.dbo.sysmail_profileaccount pa 
+	              INNER JOIN msdb.dbo.sysmail_profile p ON pa.profile_id = p.profile_id 
+	              INNER JOIN msdb.dbo.sysmail_account a ON pa.account_id = a.account_id   
+	            WHERE p.name = 'DBMAIL' 
+	              AND a.name = 'STAGCIO330DB154') 
+	  BEGIN 
+	    EXECUTE msdb.dbo.sysmail_delete_profileaccount_sp @profile_name = 'DBMAIL',@account_name = 'STAGCIO330DB154' 
+	  END  
+	IF EXISTS(SELECT * FROM msdb.dbo.sysmail_account WHERE  name = 'STAGCIO330DB154') 
+	  BEGIN 
+	    EXECUTE msdb.dbo.sysmail_delete_account_sp @account_name = 'STAGCIO330DB154' 
+	  END 
+	IF EXISTS(SELECT * FROM msdb.dbo.sysmail_profile WHERE  name = 'DBMAIL')  
+	  BEGIN 
+	    EXECUTE msdb.dbo.sysmail_delete_profile_sp @profile_name = 'DBMAIL' 
+	  END 
+	*/ 	  
---
+++ 3.0 - STIG Scan SQL2022.ps1
@@ -0,0 +1,50 @@
+﻿<#
+This script will apply STIG requirements that can be used with System Center to SQL Server 2019.
+Written by: Thomas Cook
+Updated by: Joseph Schaff
+Last Updated: 28 Mar 2019
+Updated by: Dave Rodriguez
+Last Updated: 28 Dec 2022
+Description: Updated the key path for SQL2022 from MSSQL15 to MSSQL16. 
+#>
+#Disable Adhoc Access
+$foregroundcolor = "white"
+$backgroundcolor = "green"
+$currenterror = $error[0]
+
+$currentkey = "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Lsa\FipsAlgorithmPolicy\"
+Get-ItemProperty -Path $currentkey 
+$currentkey = "HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL16.MSSQLSERVER\Providers\ADSDSOObject"
+get-ItemProperty -Path $currentkey -Name DisallowAdHocAccess -Value 1 -Type DWord
+IF ($error[0] -eq $currenterror) {Write-Host ADSDSOObject adhoc access disabled -foregroundcolor $foregroundcolor -backgroundcolor $backgroundcolor}
+$currenterror = $error[0]
+$currentkey = "HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL16.MSSQLSERVER\Providers\DB2OLEDB"
+get-ItemProperty -Path $currentkey -Name DisallowAdHocAccess -Value 1 -Type DWord
+IF ($error[0] -eq $currenterror) {Write-Host DB2OLEDB adhoc access disabled -foregroundcolor $foregroundcolor -backgroundcolor $backgroundcolor}
+$currenterror = $error[0]
+$currentkey = "HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL16.MSSQLSERVER\Providers\MSDAOSP"
+IF (-not (test-path $currentkey)) {New-Item -path $currentkey | out-null}
+get-ItemProperty -Path $currentkey -Name DisallowAdHocAccess -Value 1 -Type DWord
+IF ($error[0] -eq $currenterror) {Write-Host MSDAOSP adhoc access disabled -foregroundcolor $foregroundcolor -backgroundcolor $backgroundcolor}
+$currenterror = $error[0]
+$currentkey = "HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL16.MSSQLSERVER\Providers\MSDASQL"
+get-ItemProperty -Path $currentkey -Name DisallowAdHocAccess -Value 1 -Type DWord
+IF ($error[0] -eq $currenterror) {Write-Host MSDASQL adhoc access disabled -foregroundcolor $foregroundcolor -backgroundcolor $backgroundcolor}
+$currenterror = $error[0]
+$currentkey = "HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL16.MSSQLSERVER\Providers\MSIDXS"
+get-ItemProperty -Path $currentkey -Name DisallowAdHocAccess -Value 1 -Type DWord
+IF ($error[0] -eq $currenterror) {Write-Host MSIDXS adhoc access disabled -foregroundcolor $foregroundcolor -backgroundcolor $backgroundcolor}
+$currenterror = $error[0]
+$currentkey = "HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL16.MSSQLSERVER\Providers\MSOLAP"
+get-ItemProperty -Path $currentkey -Name DisallowAdHocAccess -Value 1 -Type DWord
+IF ($error[0] -eq $currenterror) {Write-Host MSOLAP adhoc access disabled -foregroundcolor $foregroundcolor -backgroundcolor $backgroundcolor}
+$currenterror = $error[0]
+$currentkey = "HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL15.MSSQLSERVER\Providers\SQLNCLI11"
+get-ItemProperty -Path $currentkey -Name DisallowAdHocAccess -Value 1 -Type DWord
+IF ($error[0] -eq $currenterror) {Write-Host SQLNCLI10 adhoc access disabled -foregroundcolor $foregroundcolor -backgroundcolor $backgroundcolor}
+$currenterror = $error[0]
+$currentkey = "HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL15.MSSQLSERVER\Providers\SQLOLEDB"
+IF (-not (test-path $currentkey)) {New-Item -path $currentkey | out-null}
+get-ItemProperty -Path $currentkey -Name DisallowAdHocAccess -Value 1 -Type DWord
+IF ($error[0] -eq $currenterror) {Write-Host SQLOLEDB adhoc access disabled -foregroundcolor $foregroundcolor -backgroundcolor $backgroundcolor}
+
